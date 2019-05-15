54d7c4accde09537e108ef1e812bf091575b6ffd
VINICIUS GUILHERME SILVA DOS SANTOS <vinicius.gssantos1@senacsp.edu.br>
2019-05-15 18:16:12 -0300

    Correção de Exercicios

--- ifelse.js
+++ ifelse.js
@@ -4,9 +4,9 @@ const idade = 24;
 if (nome == 'Rafael') {
     console.log('Entrou no if');
 } else if (nome == 'Vinicius') {
-    console.log('Entrou no Else if')
+    console.log('Entrou no Else if');
 } else if (nome == 'Vinicius') {
-    console.log('Entrou no Else if 2')
+    console.log('Entrou no Else if 2');
 }
  else {
     console.log('Entrou no else');
